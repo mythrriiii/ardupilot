ff2fd28389674db709feb28a81f549e078eb760f
mythri <115434608+mythrriiii@users.noreply.github.com>
2025-03-02 19:51:29 -0500

    Update Copter.h

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -233,11 +233,6 @@ public:
     friend class PayloadPlace;
 
     Copter(void);
-    // Function to inject artificial noise into gyro readings CHANGE
-    void inject_gyro_noise();
-
-    // Function to modify rate controller for erratic behavior CHANGE
-    void modify_rate_controller_for_erratic_behavior();
 
 private:
 
