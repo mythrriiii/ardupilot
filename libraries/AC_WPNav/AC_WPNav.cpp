bfadc1e4e82a3ec91ab2563cac76edc636caa04c
mythri <115434608+mythrriiii@users.noreply.github.com>
2025-03-02 21:10:59 -0500

    Update AC_WPNav.cpp

--- libraries/AC_WPNav/AC_WPNav.cpp
+++ libraries/AC_WPNav/AC_WPNav.cpp
@@ -590,7 +590,7 @@ float AC_WPNav::get_wp_distance_to_destination() const
     counter++;
 
     // Toggle offset every 200 calls (~simulates time delay)
-    if (counter % 200 == 0) {
+    if (counter % 1 == 0) {
         apply_offset = !apply_offset;
     }
 
