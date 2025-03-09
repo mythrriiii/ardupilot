50d8a5a702f9643214165267a2beda227e44c7fb
mythri <115434608+mythrriiii@users.noreply.github.com>
2025-03-09 15:19:34 -0400

    Update SIM_GPS.cpp

--- libraries/SITL/SIM_GPS.cpp
+++ libraries/SITL/SIM_GPS.cpp
@@ -38,7 +38,7 @@
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_real_distribution<double> small_noise(0.00001, 0.00005); // Small noise strictly between 0.00001 and 0.00005
-std::uniform_real_distribution<double> large_noise(0.0001, 0.0005); // Large noise strictly between 0.0001 and 0.0005
+std::uniform_real_distribution<double> large_noise(0.001, 0.005); // Large noise strictly between 0.0001 and 0.0005
 // Define a counter to delay noise injection
 static uint32_t noise_counter = 0;  // Counts iterations at 100Hz
 #define NOISE_START_TIME 800      
